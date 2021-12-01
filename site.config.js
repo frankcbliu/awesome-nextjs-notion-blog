0f82debfc1d4e6946d6d23472504061a41c6cb9c
frankcbliu <q641259875@outlook.com>
2021-12-01 11:44:34 +0800

    config: 修改配置

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'c1b2748dd0c94d9c9708751539affad6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -8,7 +8,7 @@ module.exports = {
 
   // basic site info (required)
   name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
+  domain: 'https://www.notion.so/frankcbliu/c1b2748dd0c94d9c9708751539affad6',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
