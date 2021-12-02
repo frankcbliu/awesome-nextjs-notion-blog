86a95bd63d5cbc51b2eff5e39a18464334bc5779
frankcbliu <q641259875@outlook.com>
2021-12-03 01:29:58 +0800

    fix: remove notion

--- redirects.config.js
+++ redirects.config.js
@@ -2,7 +2,7 @@ import { twitter, github, linkedin } from './site.config'
 
 const redirects = {
   calendly: `https://calendly.com/wzulfikar`,
-  notion: `https://www.notion.so/wzulfikar/dbe6b9fe47244464b1d3832969426f9e`,
+  // notion: `https://www.notion.so/wzulfikar/dbe6b9fe47244464b1d3832969426f9e`,
   twitter: `https://twitter.com/${twitter}`,
   github: `https://github.com/${github}`,
   linkedin: `https://linkedin.com/in/${linkedin}`
